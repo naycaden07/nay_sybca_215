4ff74aacbbcec6ffcb417061a387f053934cfa23
Nay Fernandes <cadennay17@gmail.com>
2024-01-20 09:48:03 +0530

    hello add

---
+++ github_215/src/github_215/add.java
@@ -0,0 +1,19 @@
+package github_215;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int add;
+		int num1=3;
+		int num2=4;
+		
+		int sum=0;
+		sum=num1+num2;
+		
+		System.out.println(sum);
+		
+	
+	}
+
+}
